066358a5576105d4ac7bf787a3a263116248b7dc
ivan <ialeksandrov1990@abv.bg>
2018-11-27 17:51:55 +0200

    Fixing lint errors

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,5 @@
 """Unit tests for calculator library"""
 
-
 import calculator
 
 
@@ -8,6 +7,7 @@ class TestCalculator:
 
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
+
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
