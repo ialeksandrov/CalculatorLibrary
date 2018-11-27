888b67a5d9df7ddc9c7b5cd05f97935c9d04b012
ivan <ialeksandrov1990@abv.bg>
2018-11-27 17:48:45 +0200

    Adding multiply function

--- calculator.py
+++ calculator.py
@@ -7,3 +7,7 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+def multiply(first_term, second_term):
+    return first_term * second_term
