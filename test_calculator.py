52db7b106eeae798a6a0504e81e0ba86056d6dda
ivan <ialeksandrov1990@abv.bg>
2018-11-27 17:55:46 +0200

    Fixing lint errors TRY 3

--- test_calculator.py
+++ test_calculator.py
@@ -7,10 +7,8 @@ class TestCalculator:
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
 
-
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
-
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
