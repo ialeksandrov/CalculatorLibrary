d35410f0f7efcfe9900d977c1d429ff6b2dd184c
ivan <ialeksandrov1990@abv.bg>
2018-11-27 17:13:20 +0200

    Add functions for addition and subtractions

---
+++ calculator.py
@@ -0,0 +1,7 @@
+"""Calculator library containing basic math operations."""
+
+def add(first_term, second_term):
+    return first_term + second_term
+
+def subtract(first_term, second_term):
+    return first_term - second_term
