25f0b2a8b81b2de6885811a5baece4ebc3b36479
ivan <ialeksandrov1990@abv.bg>
2018-11-29 16:04:24 +0200

    Adding division function and test

--- calculator.py
+++ calculator.py
@@ -11,3 +11,7 @@ def subtract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+
+def division(first_term, second_term):
+    return first_term / second_term
--- test_calculator.py
+++ test_calculator.py
@@ -12,3 +12,6 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
+
+    def test_division(self):
+        assert 1 == calculator.division(10, 10)
